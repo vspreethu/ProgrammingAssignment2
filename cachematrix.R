6de9ba9df86a93c6f53dfabd18dca88dfc41d532
Vishwanathan,Preethi <preethi.vishwanathan@cerner.com>
2015-07-26 22:50:34 -0500

    Adds function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ makeCacheMatrix <- function(x = matrix()) {
+
+      xinversion <- NULL #  result of inversion is stored
+      # A setter function, use this to set a matrix to object created by makeCacheMatrix function
+      set <- function(y) {
+	  x <<- y
+	  xinversion <<- NULL # it also initialises xinversion to null
+      }
+
+      get <- function() x # return the input matrix
+      setInv <- function(inv) xinversion <<- inv # set the inversed matrix
+      getInv <- function() xinversion # return the inversed matrix
+      list(set = set, get = get,
+	       setInv = setInv,
+	       getInv = getInv)
+  }
+
+  cacheSolve <- function(x, ...) {
+      m <- x$getInv() # get the inversed matrix from object x
+      # it will be null if uncalculated, remember the first line "xinversion <- NULL" in the previous function
+      if(!is.null(m)) { # if the inversion result is there
+	  message("getting cached data")
+	  return(m) # return the calculated inversion
+      }
+      data <- x$get() # if not, we do x$get to get the matrix object
+      m <- solve(data) # we solve it
+      x$setInv(m) # we then set it to the object
+      m # return the solved result
+  }
+
+# Test
+  # generate a random square, non-singular matrix
+  test <- matrix(runif(9,1,100),3,3)
+  # generate the makeCacheMatrix object with this matrix
+  testCached <- makeCacheMatrix(test)
+  # from now on calculate or retrieve calculated inversion using the cacheSolve function
+
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
